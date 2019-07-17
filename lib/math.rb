4702f10ac42d275093e5693acfad7142e4b36e11
Kyle Luke <th.oldschool@gmail.com>
2019-07-17 01:38:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,9 +19,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  result = (num1 % num2)
+  result = num1 % num2
+  result
 end
 
 def square_root(num)
-
+  result = num1 ** num2
+  result
 end
